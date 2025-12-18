cc6ab869b7af7dc27dc4397e0dc4c1006fb1a96c
Nathanaël Lecron <81472167+nathanael-lcr@users.noreply.github.com>
2025-12-18 16:19:38 +0100

    Update player.js
    Réparé un doublon

--- src/player.js
+++ src/player.js
@@ -9,8 +9,6 @@ export default function createPlayer(k, x = 120, y = 80) {
         area(),
         "player",
     ]);
-    const baseSpeed = 200;
-    let mvmt = baseSpeed;
 
     const baseSpeed = 200;
     const lerpFactor = 0.2;
