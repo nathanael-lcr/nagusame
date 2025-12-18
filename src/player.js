97fd219098d915a7b2182aa4f53268a07c42ad3e
Cobat <nathancobat1@gmail.com>
2025-12-18 12:28:31 +0100

    touches zqsd

--- src/player.js
+++ src/player.js
@@ -1,14 +1,20 @@
 export default function createPlayer(k, x = 120, y = 80) {
+    const SPEED = 200;
+
     const player = k.add([
         k.rect(40, 60),
         k.pos(x, y),
         "player",
     ]);
 
-    k.onKeyDown("right", () => player.move(200, 0));
-    k.onKeyDown("left",  () => player.move(-200, 0));
-    k.onKeyDown("up",    () => player.move(0, -200));
-    k.onKeyDown("down",  () => player.move(0, 200));
+    k.onKeyDown("d", () => player.move(SPEED, 0));
+    k.onKeyDown("right", () => player.move(SPEED, 0));
+    k.onKeyDown("q", () => player.move(-SPEED, 0));
+    k.onKeyDown("left", () => player.move(-SPEED, 0));
+    k.onKeyDown("z", () => player.move(0, -SPEED));
+    k.onKeyDown("up", () => player.move(0, -SPEED));
+    k.onKeyDown("s", () => player.move(0, SPEED));
+    k.onKeyDown("down", () => player.move(0, SPEED));
 
     return player;
 }
