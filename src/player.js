8e89dbc644b86fdf2bb7485dacb9287574584ec6
dejusselm <dejusselm@gmail.com>
2025-12-18 16:01:33 +0100

    Ajout du lerp

--- src/player.js
+++ src/player.js
@@ -2,13 +2,36 @@ export default function createPlayer(k, x = 120, y = 80) {
     const player = k.add([
         k.rect(40, 60),
         k.pos(x, y),
+        k.color(255, 0, 0),
         "player",
     ]);
 
-    k.onKeyDown("right", () => player.move(200, 0));
-    k.onKeyDown("left",  () => player.move(-200, 0));
-    k.onKeyDown("up",    () => player.move(0, -200));
-    k.onKeyDown("down",  () => player.move(0, 200));
+    const baseSpeed = 200;
+    const lerpFactor = 0.2;
+    let mvmt = baseSpeed;
+    let isSprinting = false;
+
+    k.onKeyDown("shift", () => {
+        isSprinting = true;
+        mvmt = baseSpeed * 2;
+    });
+    k.onKeyRelease("shift", () => {
+        isSprinting = false;
+        k.onUpdate(() => {
+            if (!isSprinting) {
+                mvmt = k.lerp(mvmt, baseSpeed, lerpFactor);
+            }
+        });
+    });
+
+    k.onKeyDown("right", () => player.move(mvmt, 0));
+    k.onKeyDown("left", () => player.move(-mvmt, 0));
+    k.onKeyDown("up", () => player.move(0, -mvmt));
+    k.onKeyDown("down", () => player.move(0, mvmt));
+
+
+
+
 
     return player;
 }
